410b5032ccd4b2fe529c7e1c502f9788b37c98b6
NikhilChoukikar <nikhilchoukikar@gmail.com>
2021-10-04 13:11:30 -0700

    Assignment 1

---
+++ Area of Rectangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+  int length = 100;
+  int width = 50;
+  int area=length*width;
+  
+  
+  printf( "Area of Rectangle = %d", area);
+  
+}  
+
---
+++ Escape Sequence Characters.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+printf(" Today's class we learnt basics of C programming \n \n");
+
+printf(" It was interactive class\b\b");
+
+printf(" \'Mam teaching method was good \a\a'" );
+
+printf(" I have made some friends " );
+
+printf(" Today we have covered Datatypes,Variables,Format specifiers, Escape sequence characters \t \t ");
+
+}
+
